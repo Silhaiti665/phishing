130b9cfc5d4635233ea2bf4b9f3589e1f8c31430
Silhaiti665 <91800038+Silhaiti665@users.noreply.github.com>
2021-10-03 23:53:56 -0400

    Add files via upload

---
+++ Website.py
@@ -0,0 +1,13 @@
+a="www.sp0m.com"
+b="www.noobtube.com"
+leo='y'
+nb=str(input("Do you wants continue[y/n]:"));
+if nb==leo:
+   print(a)
+   print('This website is for attack phishing only for hack facebook account instagram Pubg mobile freefire');
+   print(b)
+   print("This website is for learn ethnical hacking only")
+else:
+   print("contact-us by whatsapp in the number phone +50944407414")
+   quit()
+   
